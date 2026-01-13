427dcce4bfb0c00eecf3be71b981bf3bfd996824
Dima Dalloul <dima.dalloul@univ-grenoble-alpes.fr>
2026-01-13 14:30:13 +0100

    Gestion d'input d'utilisateurs

--- ESGI_B2_First_project/src/Découverte/MonPremierProgramme.java
+++ ESGI_B2_First_project/src/Découverte/MonPremierProgramme.java
@@ -2,6 +2,8 @@ package Découverte;
 
 import pizzaFactory.TaillePizza;
 
+import java.util.Scanner;
+
 public class MonPremierProgramme {
 
     public static void main (String[] args){
@@ -20,5 +22,13 @@ public class MonPremierProgramme {
          */
         TaillePizza taille = TaillePizza.M;
         System.out.println("Taille pizza choisie : " + taille);
+
+        System.out.println("Bonjour comment vas-tu aujourd'hui?");
+        Scanner scanner = new Scanner(System.in);
+        String reponseUtilisateur = scanner.nextLine();
+        System.out.println("Echo : " + reponseUtilisateur);
+        System.out.println("Un chiffre?");
+        String chiffre = "" + scanner.nextInt();
+        System.out.println(chiffre);
     }
 }
