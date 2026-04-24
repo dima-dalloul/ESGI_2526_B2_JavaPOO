9f19b9f78a6cd4c8e60b447e2b8132ea36cd5ec2
Dima Dalloul <dima.dalloul@univ-grenoble-alpes.fr>
2026-04-24 16:50:43 +0200

    Démo d'une classe générique

---
+++ ESGI_B2_Cours5/src/Box.java
@@ -0,0 +1,22 @@
+public class Box <T> {
+    private T t;
+
+    public void add(T t){
+        this.t = t;
+    }
+
+    public T get(){
+        return t;
+    }
+
+    public static void main(String[] args) {
+        Box<Integer> integerBox = new Box<Integer>();
+        Box<String> stringBox = new Box<String>();
+
+        integerBox.add(10);
+        stringBox.add("Coucou les B2!!!");
+
+        System.out.println("Integer Value: " + integerBox.get());
+        System.out.println("String Value: " + stringBox.get());
+    }
+}
